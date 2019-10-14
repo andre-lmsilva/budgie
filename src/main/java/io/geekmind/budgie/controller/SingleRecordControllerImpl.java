2b9122d6679f03dc3fe7400a0be36eeb4619c493
Andre Silva <andre.silva@hpe.com>
2019-10-14 23:28:33 +0100

    copy balance in place. keep record date and account accross new single records in place.

--- src/main/java/io/geekmind/budgie/controller/SingleRecordControllerImpl.java
+++ src/main/java/io/geekmind/budgie/controller/SingleRecordControllerImpl.java
@@ -6,20 +6,22 @@ import io.geekmind.budgie.repository.CategoryService;
 import io.geekmind.budgie.repository.SingleRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.annotation.PostConstruct;
 import javax.validation.Valid;
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/single_records")
@@ -28,6 +30,7 @@ public class SingleRecordControllerImpl {
     private final AccountService accountService;
     private final CategoryService categoryService;
     private final SingleRecordService singleRecordService;
+    private DateTimeFormatter dateFormatter;
 
     @Autowired
     public SingleRecordControllerImpl(AccountService accountService,
@@ -38,10 +41,24 @@ public class SingleRecordControllerImpl {
         this.singleRecordService = singleRecordService;
     }
 
+    @PostConstruct
+    public void initialize() {
+        this.dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    }
+
     @GetMapping("/new")
-    public ModelAndView showNewSingleRecordForm(ModelAndView requestContext) {
-        NewSingleRecord newSingleRecord = new NewSingleRecord();
-        newSingleRecord.setRecordDate(LocalDate.now());
+    public ModelAndView showNewSingleRecordForm(@RequestParam(value = "recordDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate recordDate,
+                                                @RequestParam(value = "accountId", required = false) Integer accountId,
+                                                @RequestParam(value = "recordDescription", required = false) String recordDescription,
+                                                @RequestParam(value = "recordValue", required = false) BigDecimal recordValue,
+                                                ModelAndView requestContext) {
+        NewSingleRecord newSingleRecord = new NewSingleRecord(
+            Optional.ofNullable(recordDate).orElse(LocalDate.now()),
+            accountId,
+            null,
+            recordDescription,
+            recordValue
+        );
         requestContext.addObject("newSingleRecord", newSingleRecord);
         requestContext.addObject("availableAccounts", this.accountService.loadAll());
         requestContext.addObject("availableCategories", this.categoryService.loadAll());
@@ -62,7 +79,11 @@ public class SingleRecordControllerImpl {
         } else {
             this.singleRecordService.create(newSingleRecord);
             redirectAttributes.addFlashAttribute("message", "Single record successfully created!");
-            requestContext.setViewName("redirect:/single_records/new");
+            String recordDate = this.dateFormatter.format(newSingleRecord.getRecordDate());
+
+            requestContext.setViewName(
+                String.format("redirect:/single_records/new?recordDate=%s&accountId=%s", recordDate, newSingleRecord.getAccountId())
+            );
         }
         return requestContext;
     }
