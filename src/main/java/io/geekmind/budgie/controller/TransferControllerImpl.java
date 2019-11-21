629c850a8c387dd2a100c6c83ff47beecc76ed5d
Andre Silva <andre.luis.silva@protonmail.com>
2019-11-21 21:30:21 +0000

    allow transfer to project accounts in place.

--- src/main/java/io/geekmind/budgie/controller/TransferControllerImpl.java
+++ src/main/java/io/geekmind/budgie/controller/TransferControllerImpl.java
@@ -1,18 +1,27 @@
 package io.geekmind.budgie.controller;
 
 import io.geekmind.budgie.model.dto.NewTransfer;
+import io.geekmind.budgie.model.dto.account.ExistingAccount;
+import io.geekmind.budgie.repository.ProjectAccountService;
 import io.geekmind.budgie.repository.StandardAccountService;
 import io.geekmind.budgie.repository.CategoryService;
 import io.geekmind.budgie.repository.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/transfers")
@@ -20,21 +29,27 @@ public class TransferControllerImpl {
 
     private final CategoryService categoryService;
     private final StandardAccountService standardAccountService;
+    private final ProjectAccountService projectAccountService;
     private final TransferService transferService;
 
     @Autowired
     public TransferControllerImpl(CategoryService categoryService,
                                   StandardAccountService standardAccountService,
+                                  ProjectAccountService projectAccountService,
                                   TransferService transferService) {
         this.categoryService = categoryService;
         this.standardAccountService = standardAccountService;
+        this.projectAccountService = projectAccountService;
         this.transferService = transferService;
     }
 
     @GetMapping("/new")
     public ModelAndView showNewForm(ModelAndView requestContext) {
+        Map<String, List<? extends ExistingAccount>> availableAccounts = new LinkedHashMap<>();
+        availableAccounts.put("Accounts", this.standardAccountService.loadAll());
+        availableAccounts.put("Projects", this.projectAccountService.loadAll());
+        requestContext.addObject("availableAccounts", availableAccounts);
         requestContext.addObject("availableCategories", this.categoryService.loadAll());
-        requestContext.addObject("availableAccounts", this.standardAccountService.loadNonDependantAccounts());
 
         NewTransfer newTransfer = new NewTransfer();
         newTransfer.setTransferDate(LocalDate.now());
@@ -50,8 +65,11 @@ public class TransferControllerImpl {
                                           ModelAndView requestContext,
                                           RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
+            Map<String, List<? extends ExistingAccount>> availableAccounts = new LinkedHashMap<>();
+            availableAccounts.put("Accounts", this.standardAccountService.loadAll());
+            availableAccounts.put("Projects", this.projectAccountService.loadAll());
+            requestContext.addObject("availableAccounts", availableAccounts);
             requestContext.addObject("availableCategories", this.categoryService.loadAll());
-            requestContext.addObject("availableAccounts", this.standardAccountService.loadNonDependantAccounts());
             requestContext.addObject("newTransfer", newTransfer);
             requestContext.setViewName("transfers/new.form");
         } else {
