40151f0a9ab2677b41032d25f4536f118bc49134
Andre Silva <andre.silva@zinkworks.com>
2021-10-01 14:10:06 +0100

    fixing the category summary widget

--- src/main/java/io/geekmind/budgie/repository/CategoryRepository.java
+++ src/main/java/io/geekmind/budgie/repository/CategoryRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
-    public List<Category> findByArchivedFalse();
+    List<Category> findByArchivedFalse();
 
 }
